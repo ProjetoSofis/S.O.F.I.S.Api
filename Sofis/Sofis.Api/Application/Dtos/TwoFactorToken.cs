602f1532d0efd5ee8157a52efc23cc273af1675d
ProjetoSofis <projetosofis@outlook.com>
2025-09-17 17:20:02 -0300

    feat: Two Factor entity

---
+++ Sofis/Sofis.Api/Application/Dtos/TwoFactorToken.cs
@@ -0,0 +1,11 @@
+﻿namespace Sofis.Api.Application.Dtos
+{
+    public class TwoFactorToken
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+        public bool IsUsed { get; set; }
+    }
+}
