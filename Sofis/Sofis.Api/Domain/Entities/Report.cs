ecf383bf08ed7a1f3dcc81f74a3c5b925f84c694
ProjetoSofis <projetosofis@outlook.com>
2025-09-17 15:09:43 -0300

    chore: Report entity

--- Sofis/Sofis.Api/Domain/Entities/Report.cs
+++ Sofis/Sofis.Api/Domain/Entities/Report.cs
@@ -2,5 +2,23 @@
 {
     public class Report
     {
+        public Guid Id { get; private set; }
+        public Guid EmployeeId { get; private set; }
+        public DateTime Period { get; private set; }
+
+        private readonly List<Registry> _registrys = new();
+        private IReadOnlyCollection<Registry> registrys => _registrys.AsReadOnly();
+        private Report() { }
+
+        public Report(Guid employeeId, DateTime period)
+        {
+            Id = Guid.NewGuid();
+            EmployeeId = employeeId;
+            Period = period;
+        }
+        public void Addregistry(Registry registry)
+        {
+            _registrys.Add(registry);
+        }
     }
 }
