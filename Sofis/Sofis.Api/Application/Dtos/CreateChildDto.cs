62e75dbddb48f9ca885e56830c1fa91f0b02ec10
ProjetoSofis <projetosofis@outlook.com>
2025-09-17 15:23:31 -0300

    chore: child Dto's

--- Sofis/Sofis.Api/Application/Dtos/CreateChildDto.cs
+++ Sofis/Sofis.Api/Application/Dtos/CreateChildDto.cs
@@ -2,5 +2,8 @@
 {
     public class CreateChildDto
     {
+        public string Name { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public string Responsible { get; set; } = string.Empty;
     }
 }
