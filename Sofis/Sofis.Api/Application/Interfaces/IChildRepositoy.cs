bc58ac46c8be3a0e83a0a153013eb1bbf70ee52f
GabrielNascimento25 <gabriel.pedroliva@gmail.com>
2025-09-17 15:22:24 -0300

    feat: create child repository

---
+++ Sofis/Sofis.Api/Application/Interfaces/IChildRepositoy.cs
@@ -0,0 +1,11 @@
+﻿using Sofis.Api.Domain.Entities;
+namespace Sofis.Api.Application.Interfaces
+{
+    public interface IChildRepositoy
+    {
+        Task AddAsync(Child Child);
+        Task<Child?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Child>> GetAllAsync();
+        Task DeleteAsync(Guid id);
+    }
+}
