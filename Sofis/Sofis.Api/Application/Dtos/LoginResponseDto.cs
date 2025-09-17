c0989affea5200cb82009331e05c9044575bb2d0
GabrielNascimento25 <gabriel.pedro@gmail.com>
2025-09-17 17:02:34 -0300

    feat: Created LoginResponseDto and LoginRequestDto

---
+++ Sofis/Sofis.Api/Application/Dtos/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Sofis.Api.Application.Dtos
+{
+    public class LoginResponseDto
+    {
+        public String Token { get; set; }
+        public String Username { get; set; }
+        public string Email { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+}
