db3cb7b7ea54cff0fa2b6c0a75c903b60698f22b
GabrielNascimento25 <gabriel.pedroliva@gmail.com>
2025-09-29 18:11:55 -0300

    feat: Created Update and Delete for ReportDto, RecordDto,AnnotationDto and ChildDto.

---
+++ Sofis/Sofis.Api/Application/Dtos/UpdateAnnotationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Sofis.Api.Application.Dtos
+{
+    public class UpdateAnnotationDto
+    {
+        public Guid Id { get; set; } 
+
+        public DateTime? Date { get; set; }
+
+        public string? Note { get; set; }
+    }
+}
---
+++ Sofis/Sofis.Api/Application/Dtos/UpdateRecordDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Sofis.Api.Application.Dtos
+{
+    public class UpdateRecordDto
+    {
+        public Guid Id { get; set; } 
+
+        public Guid? ChildId { get; set; }
+
+        public Guid? EmployeeId { get; set; }
+
+        public DateTime? Date { get; set; }
+    }
+}
