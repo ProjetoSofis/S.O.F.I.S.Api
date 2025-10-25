13c580b70f14d859bc7be548a44490fd1b63bf72
ProjetoSofis <projetosofis@outlook.com>
2025-10-24 23:41:36 -0300

    feat: add UpdateChildDto class for updating child information with properties for Id, name, CPF, birth date, and responsible person

---
+++ Sofis/Sofis.Api/Application/Dtos/ChildDtos/UpdateChildDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Sofis.Api.Application.Dtos.ChildDtos
+{
+    public class UpdateChildDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Responsible { get; set; }
+    }
+}
