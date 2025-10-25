30bc0a476d189b839f78f9a7eb3acd8f9872f18f
ProjetoSofis <projetosofis@outlook.com>
2025-10-24 23:33:56 -0300

    feat: add Family entity inheriting from BaseEntity with properties for family details

---
+++ Sofis/Sofis.Api/Domain/Entities/Family.cs
@@ -0,0 +1,26 @@
+﻿namespace Sofis.Api.Domain.Entities
+{
+    public class Family : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Kinship { get; private set; }
+        public string Contact { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+
+        public List<Child> RelationedChildren { get; private set; } = new();
+
+        public Family() { }
+
+        public Family(string name, string kinship, string contact, string email, string address)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Kinship = kinship;
+            Contact = contact;
+            Email = email;
+            Address = address;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
