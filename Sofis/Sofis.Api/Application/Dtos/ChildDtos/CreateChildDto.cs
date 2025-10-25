52cef6d31b2d49eac161dd70c7514197bc5d7225
ProjetoSofis <projetosofis@outlook.com>
2025-10-24 23:41:10 -0300

    feat: add CreateChildDto class for child creation with properties for name, CPF, birth date, and responsible person

---
+++ Sofis/Sofis.Api/Application/Dtos/ChildDtos/CreateChildDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Sofis.Api.Application.Dtos.ChildDtos
+{
+    public class CreateChildDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public string Responsible { get; set; } = string.Empty;
+
+
+    }
+}
+
