e4e86bb727ae8655753b1eb430c024c8eaca497a
ProjetoSofis <projetosofis@outlook.com>
2025-10-24 23:31:15 -0300

    feat: configure CORS and add child repository and service to the DI container

--- Sofis/Sofis.Api/Program.cs
+++ Sofis/Sofis.Api/Program.cs
@@ -1,22 +1,50 @@
 using Microsoft.EntityFrameworkCore;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Sofis.Api.Application.Contracts;
+using Sofis.Api.Application.Interfaces;
+using Sofis.Api.Application.Services;
 using Sofis.Api.Infrastructure;
+using Sofis.Api.Infrastructure.Persistence.Repositories;
 using Sofis.Api.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    options.ListenAnyIP(80);
+//});
+var connectionString = builder.Configuration.GetConnectionString("Default");
 
 // Add services to the container.
+builder.Services.AddScoped<IChildRepository, ChildRepository>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddScoped<IChildService, ChildService>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<SofisDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql(connectionString,
+    npgsqlOptions => npgsqlOptions.EnableRetryOnFailure(
+        maxRetryCount: 10,
+        maxRetryDelay: TimeSpan.FromSeconds(15),
+        errorCodesToAdd: null
+    )));
 
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
