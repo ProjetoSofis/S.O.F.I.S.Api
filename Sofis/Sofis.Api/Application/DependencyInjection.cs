e30bedb7138a39f3514f81ba049b6c0df5eefefe
ProjetoSofis <projetosofis@outlook.com>
2025-11-12 13:31:14 -0300

    feat: injections for reference in program.cs

---
+++ Sofis/Sofis.Api/Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Sofis.Api.Application.Contracts;
+using Sofis.Api.Application.Interfaces;
+using Sofis.Api.Application.Services;
+
+namespace Sofis.Api.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IChildService, ChildService>();
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<IPasswordService, PasswordService>();
+            services.AddScoped<IFamilyService, FamilyService>();
+            services.AddScoped<IAuthService, AuthService>();
+            return services;
+        }
+    }
+}
---
+++ Sofis/Sofis.Api/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Sofis.Api.Application.Interfaces;
+using Sofis.Api.Infrastructure.Persistence.Repositories;
+
+namespace Sofis.Api.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Default");
+            services.AddScoped<IChildRepository, ChildRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            services.AddScoped<IFamilyRepository, FamilyRepository>();
+            return services;
+        }
+    }
+}
