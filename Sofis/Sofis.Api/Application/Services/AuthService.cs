06709c84f09f40c37d1c9551f3a35c7edca00db8
ProjetoSofis <projetosofis@outlook.com>
2025-11-12 13:27:07 -0300

    feat: interface for AuthService

---
+++ Sofis/Sofis.Api/Application/Services/AuthService.cs
@@ -0,0 +1,70 @@
+﻿using Sofis.Api.Application.Contracts;
+using Sofis.Api.Application.Dtos.LoginDtos;
+using Sofis.Api.Application.Interfaces;
+using Google;
+using Google.Authenticator;
+
+namespace Sofis.Api.Application.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IPasswordService _passwordService;
+        private readonly ITokenService _tokenService;
+        public AuthService(IEmployeeRepository employeeRepository, IPasswordService passwordService, ITokenService tokenService)
+        {
+            _employeeRepository = employeeRepository;
+            _passwordService = passwordService;
+            _tokenService = tokenService;
+        }
+        public async Task<LoginResponseDto> LoginAsync(LoginDto loginDto)
+        {
+            var employee= await _employeeRepository.GetByEmailAsync(loginDto.Email);
+            if (employee == null)
+            {
+                throw new UnauthorizedAccessException("Credenciais inválidas");
+            }
+            bool isPasswordValid = _passwordService.verifyPasswordHash(loginDto.Password,
+                employee.PasswordHash, employee.PasswordSalt);
+            if (!isPasswordValid)
+            {
+                throw new UnauthorizedAccessException("Credenciais inválidas");
+            }
+            if (employee.IsTwoFactorEnabled)
+            {
+                return new LoginResponseDto
+                {
+                    IsTwoFactorRequired = true
+                };
+            }
+
+            var token = _tokenService.GenerateToken(employee);
+            return new LoginResponseDto
+            {
+                IsTwoFactorRequired = false,
+                AcessToken = token
+            };
+        }
+
+        public async Task<LoginResponseDto> VerifyTwoFactor(TwoFactorLoginDto twoFactorLoginDto)
+        {
+            var employee = await _employeeRepository.GetByEmailAsync(twoFactorLoginDto.Email);
+            if (employee == null || !employee.IsTwoFactorEnabled || string.IsNullOrEmpty(employee.TwoFactorSecret))
+            {
+                throw new UnauthorizedAccessException("Verificação 2FA falhou ou 2FA não está configurado");
+            }
+            var tfa = new TwoFactorAuthenticator();
+            bool isCodeValid = tfa.ValidateTwoFactorPIN(employee.TwoFactorSecret, twoFactorLoginDto.TwoFactorCode);
+            if (!isCodeValid)
+            {
+                throw new UnauthorizedAccessException("Código 2FA inválido ou expirado");
+            }
+            var token = _tokenService.GenerateToken(employee);
+            return new LoginResponseDto
+            {
+                IsTwoFactorRequired = false,
+                AcessToken = token
+            };
+        }
+    }
+}
