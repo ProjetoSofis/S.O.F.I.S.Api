11138cdb44a694f45ee9dccb2ed5d484a391a295
ProjetoSofis <projetosofis@outlook.com>
2025-11-12 13:25:24 -0300

    feat: add DTOs for login functionality including LoginDto, LoginResponseDto, and TwoFactorLoginDto

---
+++ Sofis/Sofis.Api/Application/Dtos/LoginDtos/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sofis.Api.Application.Dtos.LoginDtos
+{
+    public class LoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Sofis/Sofis.Api/Application/Dtos/LoginDtos/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Sofis.Api.Application.Dtos.LoginDtos
+{
+    public class LoginResponseDto
+    {
+        public bool IsTwoFactorRequired { get; set; } = false;
+        public string? AcessToken { get; set; }
+    }
+}
