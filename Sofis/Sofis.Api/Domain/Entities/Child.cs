c2d4f49babd2311429d386b9fbbcba8e1105873b
ProjetoSofis <projetosofis@outlook.com>
2025-11-12 13:00:29 -0300

    feat: update Child entity to change BirthDate type to DateOnly and add new properties

--- Sofis/Sofis.Api/Domain/Entities/Child.cs
+++ Sofis/Sofis.Api/Domain/Entities/Child.cs
@@ -1,7 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Sofis.Api.Domain.Entities
 {
+    public enum Status
+    {
+        Ativo,
+        Inativo
+    }
     public class Child : BaseEntity
     {
         public string Name { get; set; }
@@ -9,10 +15,13 @@ namespace Sofis.Api.Domain.Entities
         public string Cpf { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime BirthDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}", ApplyFormatInEditMode = true)]
+        public DateOnly BirthDate { get; set; }
         public string Responsible { get; set; }
 
+        public string MomName { get; set; }
+        public string DadName { get; set; }
+        public Status Status { get; set; }
         public List<Family> FamilyMembers { get; private set; } = new();
 
         public readonly List<Report> _annotations = new();
@@ -20,16 +29,19 @@ namespace Sofis.Api.Domain.Entities
 
         public Child() { }
 
-        public Child(string name, DateTime birthDate, string responsible)
+        public Child(string name, DateOnly birthDate, string responsible, string momName, string dadName, Status status)
         {
             Id = Guid.NewGuid();
             Name = name;
             BirthDate = birthDate;
             Responsible = responsible;
+            MomName = momName;
+            DadName = dadName;
+            Status = status;
             CreatedAt = DateTime.UtcNow;
         }
 
-        //public void AddAnnotation(string text, DateTime date, Guid employeeId)
+        //public void AddAnnotation(string text, DateOnly date, Guid employeeId)
         //{
         //    _annotations.Add(new Report(employeeId, date, text));
         //}
