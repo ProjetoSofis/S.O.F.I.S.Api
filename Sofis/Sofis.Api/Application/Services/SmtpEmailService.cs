e702edb649adca49603d5dd1e6c6f540aed91a1a
ProjetoSofis <projetosofis@outlook.com>
2025-11-13 12:27:45 -0300

    feat: logic for send email in 2FA

---
+++ Sofis/Sofis.Api/Application/Services/SmtpEmailService.cs
@@ -0,0 +1,34 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using Sofis.Api.Application.Contracts;
+
+namespace Sofis.Api.Application.Services
+{
+    public class SmtpEmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+        public SmtpEmailService(IConfiguration _configuration)
+        {
+            _configuration = _configuration;
+        }
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_configuration["Smtp:FromEmail"]));
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+
+            await smtp.ConnectAsync(
+                _configuration["Smtp:Host"],
+                int.Parse(_configuration["Smtp:Port"]),
+                SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(_configuration["Smtp:Username"], _configuration["Smtp:Password"]);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+}
