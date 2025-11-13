bae0737fceaecb538a9e2f79d7b84017c06017de
ProjetoSofis <projetosofis@outlook.com>
2025-11-13 12:27:10 -0300

    feat: add TokenService

--- Sofis/Sofis.Api/Application/DependencyInjection.cs
+++ Sofis/Sofis.Api/Application/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Sofis.Api.Application.Contracts;
 using Sofis.Api.Application.Interfaces;
 using Sofis.Api.Application.Services;
+using Sofis.Api.Infrastructure.Auth;
 
 namespace Sofis.Api.Application
 {
@@ -12,7 +13,9 @@ namespace Sofis.Api.Application
             services.AddScoped<IEmployeeService, EmployeeService>();
             services.AddScoped<IPasswordService, PasswordService>();
             services.AddScoped<IFamilyService, FamilyService>();
+            services.AddSingleton<ITokenService, TokenService>();
             services.AddScoped<IAuthService, AuthService>();
+
             return services;
         }
     }
