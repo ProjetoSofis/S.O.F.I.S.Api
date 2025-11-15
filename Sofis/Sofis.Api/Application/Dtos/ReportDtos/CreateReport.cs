07564062f0982c4d8f217c85a3f48d4582f66950
ProjetoSofis <projetosofis@outlook.com>
2025-11-15 00:07:38 -0300

    feat: Dto for create Report

---
+++ Sofis/Sofis.Api/Application/Dtos/ReportDtos/CreateReport.cs
@@ -0,0 +1,11 @@
+﻿namespace Sofis.Api.Application.Dtos.ReportDtos
+{
+    public class CreateReport
+    {
+        public Guid EmployeeId { get; set; }
+        public Guid ChildId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
