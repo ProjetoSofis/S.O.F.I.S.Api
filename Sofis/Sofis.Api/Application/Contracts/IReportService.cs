b1dd0e04ef88b21259a5bcc3ab60d323b3d3ec0e
ProjetoSofis <projetosofis@outlook.com>
2025-11-17 18:05:57 -0300

    feat: added methods for report service

---
+++ Sofis/Sofis.Api/Application/Contracts/IReportService.cs
@@ -0,0 +1,13 @@
+﻿using Sofis.Api.Application.Dtos.ReportDtos;
+
+namespace Sofis.Api.Application.Contracts
+{
+    public interface IReportService
+    {
+        Task<ReportDto> CreateReport(CreateReportDto dto);
+        Task<IEnumerable<ReportDto>> GetReportsByChildId(Guid childId);
+        Task<ReportDto> GetReportsById(Guid id);
+        Task<ReportDto?> UpdateReport(Guid id, UpdateReportDto report);
+        Task DeleteReport(Guid id);
+    }
+}
