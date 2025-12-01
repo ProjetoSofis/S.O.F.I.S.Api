0ab6bfffaf602b8980530d08cf6c7160c2a9e396
ProjetoSofis <projetosofis@outlook.com>
2025-12-01 00:42:14 -0300

    feat: implemented method of relationship child with family in controller

--- Sofis/Sofis.Api/Controllers/FamilyController.cs
+++ Sofis/Sofis.Api/Controllers/FamilyController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sofis.Api.Application.Contracts;
 using Sofis.Api.Application.Dtos.FamilyDtos;
+using Sofis.Api.Application.Dtos.GuardianDtos;
 
 namespace Sofis.Api.Controllers
 {
@@ -16,46 +17,12 @@ namespace Sofis.Api.Controllers
             this._familyService = familyService;
             this._logger = logger;
         }
-        [HttpGet]
-        public async Task<IActionResult> GetAllFamilies()
+        [HttpPost]
+        [Route("guardian")]
+        public async Task<IActionResult> RegisterGuardian([FromBody] CreateGuardianDto dto)
         {
-            try
-            {
-                var families = await _familyService.GetAllAsync();
-                return Ok(families);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error retrieving families.");
-                return StatusCode(500, new
-                {
-                    error = "Internal server error.",
-                    details = ex.Message
-                });
-            }
-        }
-        [HttpGet]
-        [Route("cpf/{cpf}")]
-        public async Task<IActionResult> GetFamilyByCpf(string cpf)
-        {
-            try
-            {
-                var family = await _familyService.GetByCpf(cpf);
-                if (family == null)
-                {
-                    return NotFound();
-                }
-                return Ok(family);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error retrieving family by CPF.");
-                return StatusCode(500, new
-                {
-                    error = "Internal server error.",
-                    details = ex.Message
-                });
-            }
+            var result = await _familyService.RegisterGuardianForChildAsync(dto);
+            return CreatedAtAction(nameof(GetFamilyById), new { id = result.Id }, result);
         }
         [HttpGet]
         [Route("{id}")]
@@ -63,7 +30,7 @@ namespace Sofis.Api.Controllers
         {
             try
             {
-                var family = await _familyService.GetByIdAsync(id);
+                var family = await _familyService.GetGuardianByIdAsync(id);
                 if (family == null)
                 {
                     return NotFound();
@@ -79,61 +46,126 @@ namespace Sofis.Api.Controllers
                     details = ex.Message
                 });
             }
-        }
-        [HttpPost]
-        public async Task<IActionResult> RegisterFamily([FromBody] CreateFamilyDto dto)
-        {
-            try
-            {
-                var family = await _familyService.RegisterFamilyAsync(dto);
-                return CreatedAtAction(nameof(GetFamilyById), new { id = family.Id }, family);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error registering family.");
-                return StatusCode(500, new
-                {
-                    error = "Internal server error.",
-                    details = ex.Message
-                });
-            }
-        }
-        [HttpPut]
-        public async Task<IActionResult> UpdateFamily([FromBody] UpdateFamilyDto dto)
-        {
-            try
-            {
-                var family = await _familyService.UpdateFamilyAsync(dto);
-                return Ok(family);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error updating family.");
-                return StatusCode(500, new
-                {
-                    error = "Internal server error.",
-                    details = ex.Message
-                });
-            }
-        }
-        [HttpDelete]
-        [Route("{id}")]
-        public async Task<IActionResult> DeleteFamily(Guid id)
-        {
-            try
-            {
-                await _familyService.DeleteFamilyAsync(id);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error deleting family.");
-                return StatusCode(500, new
-                {
-                    error = "Internal server error.",
-                    details = ex.Message
-                });
-            }
+            //}
+            //[HttpGet]
+            //public async Task<IActionResult> GetAllFamilies()
+            //{
+            //    try
+            //    {
+            //        var families = await _familyService.GetAllAsync();
+            //        return Ok(families);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        _logger.LogError(ex, "Error retrieving families.");
+            //        return StatusCode(500, new
+            //        {
+            //            error = "Internal server error.",
+            //            details = ex.Message
+            //        });
+            //    }
+            //}
+            //[HttpGet]
+            //[Route("cpf/{cpf}")]
+            //public async Task<IActionResult> GetFamilyByCpf(string cpf)
+            //{
+            //    try
+            //    {
+            //        var family = await _familyService.GetByCpf(cpf);
+            //        if (family == null)
+            //        {
+            //            return NotFound();
+            //        }
+            //        return Ok(family);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        _logger.LogError(ex, "Error retrieving family by CPF.");
+            //        return StatusCode(500, new
+            //        {
+            //            error = "Internal server error.",
+            //            details = ex.Message
+            //        });
+            //    }
+            //}
+            //[HttpGet]
+            //[Route("{id}")]
+            //public async Task<IActionResult> GetFamilyById(Guid id)
+            //{
+            //    try
+            //    {
+            //        var family = await _familyService.GetByIdAsync(id);
+            //        if (family == null)
+            //        {
+            //            return NotFound();
+            //        }
+            //        return Ok(family);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        _logger.LogError(ex, "Error retrieving family by ID.");
+            //        return StatusCode(500, new
+            //        {
+            //            error = "Internal server error.",
+            //            details = ex.Message
+            //        });
+            //    }
+            //}
+            //[HttpPost]
+            //public async Task<IActionResult> RegisterFamily([FromBody] CreateFamilyDto dto)
+            //{
+            //    try
+            //    {
+            //        var family = await _familyService.RegisterFamilyAsync(dto);
+            //        return CreatedAtAction(nameof(GetFamilyById), new { id = family.Id }, family);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        _logger.LogError(ex, "Error registering family.");
+            //        return StatusCode(500, new
+            //        {
+            //            error = "Internal server error.",
+            //            details = ex.Message
+            //        });
+            //    }
+            //}
+            //[HttpPut]
+            //public async Task<IActionResult> UpdateFamily([FromBody] UpdateFamilyDto dto)
+            //{
+            //    try
+            //    {
+            //        var family = await _familyService.UpdateFamilyAsync(dto);
+            //        return Ok(family);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        _logger.LogError(ex, "Error updating family.");
+            //        return StatusCode(500, new
+            //        {
+            //            error = "Internal server error.",
+            //            details = ex.Message
+            //        });
+            //    }
+            //}
+            //[HttpDelete]
+            //[Route("{id}")]
+            //public async Task<IActionResult> DeleteFamily(Guid id)
+            //{
+            //    try
+            //    {
+            //        await _familyService.DeleteFamilyAsync(id);
+            //        return NoContent();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        _logger.LogError(ex, "Error deleting family.");
+            //        return StatusCode(500, new
+            //        {
+            //            error = "Internal server error.",
+            //            details = ex.Message
+            //        });
+            //    }
+            //}
         }
     }
 }
