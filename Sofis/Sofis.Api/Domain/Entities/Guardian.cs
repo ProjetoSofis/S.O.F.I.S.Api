581b3a185c34b76cd2f874b972f0e916034f9017
ProjetoSofis <projetosofis@outlook.com>
2025-12-01 00:11:31 -0300

    feat: created guardian entity

---
+++ Sofis/Sofis.Api/Domain/Entities/Guardian.cs
@@ -0,0 +1,16 @@
+﻿namespace Sofis.Api.Domain.Entities
+{
+    public class Guardian : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Kinship { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public Guid FamilyId { get; set; }
+        public Family Family { get; set; }
+
+    }
+}
