87b3795d3caa209aeeee378e1977b459370e5937
ProjetoSofis <projetosofis@outlook.com>
2025-12-01 16:47:50 -0300

    feat: created guardian services methods

---
+++ Sofis/Sofis.Api/Application/Contracts/IGuardianService.cs
@@ -0,0 +1,13 @@
+﻿using Sofis.Api.Application.Dtos.GuardianDtos;
+
+namespace Sofis.Api.Application.Contracts
+{
+    public interface IGuardianService
+    {
+        Task<GuardianDto> CreateGuardianAsync(CreateGuardianDto dto);
+        Task<ICollection<GuardianDto>> GetAllGuardiansAsync();
+        Task<GuardianDto?> GetGuardianByIdAsync(Guid id);
+        Task UpdateGuardianAsync(Guid id, CreateGuardianDto dto);
+        Task DeleteGuardianAsync(Guid id);
+    }
+}
