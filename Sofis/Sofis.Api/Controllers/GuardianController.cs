80ed62008fc241be0f76834f3d7eb75a2bc8ebf3
ProjetoSofis <projetosofis@outlook.com>
2025-12-01 16:54:44 -0300

    feat: guardian controller for http methods

---
+++ Sofis/Sofis.Api/Controllers/GuardianController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sofis.Api.Application.Contracts;
+using Sofis.Api.Application.Dtos.GuardianDtos;
+
+namespace Sofis.Api.Controllers
+{
+    public class GuardianController : ControllerBase
+    {
+        private readonly IGuardianService _guardianService;
+        private readonly ILogger<GuardianController> _logger;
+        public GuardianController(IGuardianService guardianService, ILogger<GuardianController> logger)
+        {
+            this._guardianService = guardianService;
+            this._logger = logger;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateGuardian([FromBody] CreateGuardianDto dto)
+        {
+            try
+            {
+                var createdGuardian = await _guardianService.CreateGuardianAsync(dto);
+                return CreatedAtAction(nameof(GetById), new { id = createdGuardian.Id }, createdGuardian);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error creating guardian.");
+                return StatusCode(500, new
+                {
+                    error = "Internal server error.",
+                    details = ex.Message
+                });
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                var guardian = await _guardianService.GetGuardianByIdAsync(id);
+                if (guardian == null)
+                {
+                    return NotFound();
+                }
+                return Ok(guardian);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving guardian by ID.");
+                return StatusCode(500, new
+                {
+                    error = "Internal server error.",
+                    details = ex.Message
+                });
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllGuardians()
+        {
+            try
+            {
+                var guardians = await _guardianService.GetAllGuardiansAsync();
+                return Ok(guardians);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving guardians.");
+                return StatusCode(500, new
+                {
+                    error = "Internal server error.",
+                    details = ex.Message
+                });
+            }
+        }
+    }
+}
