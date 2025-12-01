44e7c48128a8be503105f869bb85c2ccfcd3a05e
ProjetoSofis <projetosofis@outlook.com>
2025-12-01 01:24:52 -0300

    fix: expected }

--- Sofis/Sofis.Api/Domain/Entities/Family.cs
+++ Sofis/Sofis.Api/Domain/Entities/Family.cs
@@ -2,7 +2,7 @@
 {
     public class Family : BaseEntity
     {
-        public string SurName { get; set; 
+        public string SurName { get; set; } 
 
        public ICollection<Child> RelationedChildren { get; set; }
        public ICollection<Guardian> Guardians { get; set; }
