ff8d79c6441e4bfac163aec56c22b266531ec91f
ProjetoSofis <projetosofis@outlook.com>
2025-12-01 00:06:44 -0300

    feat: unified injection dependencies

--- Sofis/Sofis.Api/Infrastructure/DependencyInjection.cs
+++ Sofis/Sofis.Api/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Sofis.Api.Application.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using Sofis.Api.Application.Contracts;
 using Sofis.Api.Application.Interfaces;
 using Sofis.Api.Application.Services;
 using Sofis.Api.Infrastructure.Auth;
@@ -10,11 +11,18 @@ namespace Sofis.Api.Infrastructure
     {
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("Default");
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            
+            services.AddDbContext<SofisDbContext>(options =>
+                options.UseNpgsql(connectionString));
+
             services.AddScoped<IChildRepository, ChildRepository>();
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             services.AddScoped<IFamilyRepository, FamilyRepository>();
+            services.AddScoped<IGuardianRepository, GuardianRepository>();
             services.AddScoped<IEmailService, SmtpEmailService>();
+            services.AddScoped<IPasswordService, PasswordService>();
+            services.AddScoped<ITokenService, TokenService>();
             return services;
         }
     }
