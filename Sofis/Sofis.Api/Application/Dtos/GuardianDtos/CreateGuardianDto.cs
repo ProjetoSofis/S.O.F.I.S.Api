d4d6ca0823053876e627d64ea2dc0f0951d4281d
ProjetoSofis <projetosofis@outlook.com>
2025-12-01 16:45:57 -0300

    feat: removed wrong attribute and added familyId

--- Sofis/Sofis.Api/Application/Dtos/GuardianDtos/CreateGuardianDto.cs
+++ Sofis/Sofis.Api/Application/Dtos/GuardianDtos/CreateGuardianDto.cs
@@ -4,10 +4,8 @@
     {
         public string Name { get; set; }
         public string Cpf { get; set; }
-        public string Kinship { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public string Address { get; set; }
-        public Guid ChildId { get; set; }
+        public Guid FamilyId { get; set; }
     }
 }
