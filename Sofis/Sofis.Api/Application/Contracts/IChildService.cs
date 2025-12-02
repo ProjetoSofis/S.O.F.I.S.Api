5e7216b283c345524627d7497be60af2361bb60b
ProjetoSofis <projetosofis@outlook.com>
2025-12-01 23:43:18 -0300

    feat: removed unused methods

--- Sofis/Sofis.Api/Application/Contracts/IChildService.cs
+++ Sofis/Sofis.Api/Application/Contracts/IChildService.cs
@@ -9,8 +9,6 @@ namespace Sofis.Api.Application.Contracts
         Task<IEnumerable<ChildDto>> GetAllAsync();
         Task<ChildDto?> GetByIdAsync(Guid id);
         Task<ChildDto?> GetByCpfASync(string cpf);
-        Task<IEnumerable<ChildDto>> GetFamilyMembersByChildIdAsync(Guid childId, Guid familyId);
-        Task AssignFamilyToChildAsync(Guid childId, Guid familyId);
         Task<IEnumerable<ChildDto>> GetByNameAsync(string name);
         Task<ChildDto> UpdateChildAsync(Guid id, UpdateChildDto dto);
         Task DeleteChildAsync(Guid id);
