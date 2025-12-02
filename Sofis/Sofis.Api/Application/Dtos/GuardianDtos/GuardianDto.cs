a192eb59af207362534f7204192525bc17b6acb2
ProjetoSofis <projetosofis@outlook.com>
2025-12-02 12:28:53 -0300

    feat: added familyName in DTO

--- Sofis/Sofis.Api/Application/Dtos/GuardianDtos/GuardianDto.cs
+++ Sofis/Sofis.Api/Application/Dtos/GuardianDtos/GuardianDto.cs
@@ -8,5 +8,6 @@
         public string Phone { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public Guid FamilyId { get; set; }
+        public  string FamilyName { get; set; } = string.Empty;
     }
 }
