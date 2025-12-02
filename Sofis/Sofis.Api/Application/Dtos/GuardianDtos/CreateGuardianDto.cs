157872176e5c659b93375f0997d05d44a04847f1
ProjetoSofis <projetosofis@outlook.com>
2025-12-02 15:15:31 -0300

    feat: new DTO attributes

--- Sofis/Sofis.Api/Application/Dtos/GuardianDtos/CreateGuardianDto.cs
+++ Sofis/Sofis.Api/Application/Dtos/GuardianDtos/CreateGuardianDto.cs
@@ -4,8 +4,9 @@
     {
         public string Name { get; set; }
         public string Cpf { get; set; }
+        public string Kinship { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public Guid FamilyId { get; set; }
+        public Guid ChildId { get; set; }
     }
 }
--- Sofis/Sofis.Api/Application/Dtos/GuardianDtos/GuardianDto.cs
+++ Sofis/Sofis.Api/Application/Dtos/GuardianDtos/GuardianDto.cs
@@ -5,9 +5,9 @@
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Cpf { get; set; } = string.Empty;
+        public string Kinship { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public Guid FamilyId { get; set; }
-        public  string FamilyName { get; set; } = string.Empty;
+        public Guid ChildId { get; set; }
     }
 }
