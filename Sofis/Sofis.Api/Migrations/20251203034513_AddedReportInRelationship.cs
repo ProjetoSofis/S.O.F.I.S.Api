b1a2a3a11217959f7397764ff7cb06dc2b5578aa
ProjetoSofis <projetosofis@outlook.com>
2025-12-03 00:45:50 -0300

    feat: added new informations in Migrations

---
+++ Sofis/Sofis.Api/Migrations/20251203034513_AddedReportInRelationship.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sofis.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedReportInRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
