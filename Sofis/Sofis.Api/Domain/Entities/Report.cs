36139c89b3b2e9610ebf930f31d955bba9a3f995
vin1pe <viniciuspelissari2707@gmail.com>
2025-12-02 23:49:58 -0300

    Fix: Add 'title' parameter to constructor and resolve CS0103 error.

--- Sofis/Sofis.Api/Domain/Entities/Report.cs
+++ Sofis/Sofis.Api/Domain/Entities/Report.cs
@@ -3,18 +3,19 @@
     public class Report : BaseEntity
     {
         public Guid EmployeeId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public Employee Employee { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Employee? Employee { get; set; }
         public Guid ChildId { get; set; }
-        public Child Child { get; set; }
+        public Child? Child { get; set; }
 
         public Report() { }
 
-        public Report(Guid employeeId, string description, Guid childId)
+        public Report(Guid employeeId, string title,string description, Guid childId)
         {
             Id = Guid.NewGuid();
             EmployeeId = employeeId;
+            Title = title;
             ChildId = childId;
             CreatedAt = DateTime.UtcNow;
             Description = description;
